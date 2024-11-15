ef48f9a8a44175c23498a6a423baa0f42e07abb6
JeruFaia <142933351+JeruFaia@users.noreply.github.com>
2024-11-15 20:59:33 -0300

    Add files via upload
    Añadido el código para exportar a pdf
    no está probado nada

--- src/app/pages/tabs-home/tab-tres/tab-tres.page.ts
+++ src/app/pages/tabs-home/tab-tres/tab-tres.page.ts
@@ -1,8 +1,9 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
-import { IonBackButton, IonModal, IonAvatar, IonList, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonImg, IonButton, IonButtons, IonMenuButton, IonMenu, IonIcon, IonContent, IonHeader, IonInput, IonInputPasswordToggle, IonItem, IonLabel, IonRouterLink, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { LoginFormPage } from '../../auth/login-form/login-form.page';
+import { Directory, Filesystem, WriteFileOptions } from '@capacitor/filesystem';
+import { jsPDF } from 'jspdf';
+import { FormsModule, FormGroup, ReactiveFormsModule, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { IonBackButton, IonSelect, IonSelectOption, IonCheckbox, IonModal, IonAvatar, IonList, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonImg, IonButton, IonButtons, IonMenuButton, IonMenu, IonIcon, IonContent, IonHeader, IonInput, IonInputPasswordToggle, IonItem, IonLabel, IonRouterLink, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
@@ -10,43 +11,111 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './tab-tres.page.html',
   styleUrls: ['./tab-tres.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader,IonItem, IonTitle, IonToolbar, ReactiveFormsModule, CommonModule, FormsModule,IonRouterLink, RouterLink,IonBackButton,IonModal,IonImg,IonButton,IonButtons,IonMenu,IonMenuButton,IonItem,IonInput,IonLabel,IonIcon,IonAvatar,IonList,IonCard,IonCardContent,IonCardTitle,IonCardSubtitle,IonCardHeader,IonInputPasswordToggle,LoginFormPage
-  ],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, ReactiveFormsModule, CommonModule, FormsModule,
+    IonRouterLink, RouterLink,
+    IonBackButton,
+    IonCheckbox,
+    IonSelect,
+    IonSelectOption,
+    IonModal,
+    IonImg,
+    IonButton,
+    IonButtons,
+    IonMenu,
+    IonMenuButton,
+    IonItem,
+    IonInput,
+    IonLabel,
+    IonIcon,
+    IonAvatar,
+    IonList,
+    IonCard,
+    IonCardContent,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonInputPasswordToggle,
+    //LoginFormPage
+  ]
   })
 export class TabTresPage implements OnInit {
 
   
-  registroLibreta: FormGroup = new FormGroup({
-    nombre: new FormControl<string | null>('', [Validators.required, Validators.minLength(3)]),
-    raza: new FormControl<string | null>('', [Validators.required, Validators.minLength(3)]),
-    fechaNacimiento: new FormControl<string | null>('', [Validators.required]),
-    sexo: new FormControl<string | null>('', [Validators.required]),
-    tipoMascosta: new FormControl<string | null>('', [Validators.required]),
-    enfermedad: new FormControl<string | null >('', [Validators.required]),
-    vacunas: new FormControl<string | null>('', [Validators.required]),
-    veterinaria: new FormControl<string | null>('', [Validators.required]),
-    nomPropietario: new FormControl<string | null>('', [Validators.required, Validators.minLength(4)]),
-    apelPropietario: new FormControl<string | null>('', [Validators.required, Validators.minLength(4)]),
-    direccion: new FormControl<string | null>('', [Validators.required]),
-    telefono: new FormControl<string | null>('', [Validators.required]),  
-    celular: new FormControl<string | null>('', [Validators.required])
-  })
+  registroLibreta!: FormGroup; 
 
-  constructor(private router: Router) { }
+  private libretaCompleta: any[] = []
+
+  constructor(private formBuilder: FormBuilder,
+    private router: Router) { }
 
    
 
   ngOnInit() {
-    this.registroLibreta
+    
+    this.registroLibreta = this.formBuilder.group({
+    veterinaria: new FormControl<string | null>('', [Validators.required]),
+    diagnostico: new FormControl<string | null>('', [Validators.required]),
+    libreta: new FormControl<string | null>('', [Validators.required]),
+    tratamiento: new FormControl<string | null>('', [Validators.required]),
+    turno: new FormControl<string | null>('', [Validators.required])
+  })
+   this.registroLibreta
     this.completarLibreta()
   }
 
-  completarLibreta(){}
 
-  isValid(){
+  completarLibreta(){
+    const completarRegistro = this.registroLibreta.value;
 
+    this.libretaCompleta.push(completarRegistro);
+    console.log('Libreta Completa:', completarRegistro);
+    console.log('lista:', this.libretaCompleta);
   }
+
+  isValid(): boolean {
+    const formValues = this.registroLibreta.value;
+    return formValues.veterinaria.length > 0 && formValues.diagnostico.length > 0 && formValues.libreta.length > 0 && formValues.tratamiento > 0 && formValues.turno !== this.registroLibreta; 
+    };
+
+  /////////////////////////////////////////////////////////////////////////////////////
+  //Exportación a PDF
+
+  //IMPORTANTE: añadir los siguientes permisos en el android.manifest:
+  // <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
+  // <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+   async formularioPDF(): Promise<void> {
+    const pdfFormulario = 'base64/PDF-base64.pdf';
+    const contentBase64= 'JVBERi0xLjQKJaqrrK0KMSAwIG9iago8PAovVGl0bGUgKF...';
+
+    const fileOptions: WriteFileOptions = {
+      path: pdfFormulario,
+      data: contentBase64,
+      directory: Directory.Documents,
+      recursive: true
+    };
+
+    await Filesystem.writeFile(fileOptions);
+  }
+
+  //guardar en string un pdf
+  /*async creaPdfDesdeTexto() {
+    const formularioAPdf: "text/plane.text.pdf"
+    const text = this.registroLibreta.value;
+    const pdf = new jsPDF();
+    pdf.text(text, 10, 10);
+    const pdfOutput = pdf.output('datauristring');
+    await this.savePDF(formularioAPdf, pdfOutput)
+    
+}*/
+  //////////////////////////////////////////////////////////////////////
+
   onSubmit(){
-    console.log(this.registroLibreta.value)
+    if(this.registroLibreta.valid){
+    this.completarLibreta();
+    console.log('Formulario enviado:', this.registroLibreta.value);
+    }else{
+      alert('Por favor, complete todos los datos')
+    }
+    
   }
 }
